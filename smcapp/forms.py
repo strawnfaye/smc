501dfffed19e68cb1c3f58c6a7d46345f70fef0b
Natacha Barcala <nabarcala@gmail.com>
2017-11-09 17:24:50 -0500

    Added a registration page

--- smcapp/forms.py
+++ smcapp/forms.py
@@ -1,7 +1,22 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 class LoginForm(forms.Form):
 	MAX_LENGTH = 20
 
 	email = forms.EmailField(label='Username', max_length=MAX_LENGTH, widget=forms.TextInput(attrs={'style':'width:300;'}))
 	password = forms.CharField(label='Password', max_length=MAX_LENGTH)
+
+class SignUpForm(UserCreationForm):
+	MAX_LENGTH = 20
+	
+	username = forms.EmailField(max_length=150, required=True, help_text='Required. Enter a valid email address.')
+	first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+	last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+	password1 = forms.CharField(label='Password', max_length=MAX_LENGTH)
+	password2 = forms.CharField(label='Verify Password', max_length=MAX_LENGTH) 
+
+	class Meta:
+		model = User
+		fields = ('username', 'first_name', 'last_name', 'password1', 'password2')
--- smcapp/views.py
+++ smcapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
+from smcapp.forms import SignUpForm
 from .forms import LoginForm
 
 def index(request):
@@ -24,3 +27,20 @@ def index(request):
 		form = LoginForm()
 
 	return render(request, 'smcapp/index.htm', {'form': form})
+
+def register(request):
+	if request.method == 'POST':
+		form = SignUpForm(request.POST)
+		# once the information is check for being "unique" save the information
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data.get('username')
+			raw_password = form.cleaned_data.get('password1')
+			user = authenticate(username=username, password=raw_password)
+			login(request, user)
+			return HttpResponseRedirect('/admin/')
+	# else create a user form to register
+	else:
+		form = SignUpForm()
+		args = {'form': form}
+		return render(request, 'smcapp/reg_form.htm', args) 
