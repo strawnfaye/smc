bfc0610b6d2127b5d0816497fd792ea222f872cc
Faye <strawnfaye@gmail.com>
2017-11-13 15:34:44 -0500

    updated some CSS

--- smcapp/forms.py
+++ smcapp/forms.py
@@ -10,12 +10,12 @@ class LoginForm(forms.Form):
 
 class SignUpForm(UserCreationForm):
 	MAX_LENGTH = 20
-	
+
 	username = forms.EmailField(max_length=150, required=True, help_text='Required. Enter a valid email address.')
 	first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
 	last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
 	password1 = forms.CharField(label='Password', max_length=MAX_LENGTH)
-	password2 = forms.CharField(label='Verify Password', max_length=MAX_LENGTH) 
+	password2 = forms.CharField(label='Verify Password', max_length=MAX_LENGTH)
 
 	class Meta:
 		model = User
